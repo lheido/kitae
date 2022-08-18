8b5d6c9514a50a8b87d1b351efd459179ecc00a4
lheido <leonard.treille@gmail.com>
2022-08-18 16:13:35 +0000

    feat(designer): Finish the login page UI

---
+++ packages/kitae-designer/src/assets/DecoLeft.tsx
@@ -0,0 +1,32 @@
+export const DecoLeft = () => {
+  const width = window.innerWidth / 2;
+  const angleX = width / 1.7;
+  return (
+    <svg width={width} height="206" viewBox={`0 0 ${width} 206`}>
+      <defs>
+        <linearGradient id="DecoLeftGradient" gradientUnits="userSpaceOnUse">
+          <stop offset="5%" stopColor="#1D8660" />
+          <stop offset="95%" stopColor="#1D6686" />
+        </linearGradient>
+      </defs>
+      <path
+        d={`M0 2 H ${angleX} L ${width} 164`}
+        fill="transparent"
+        stroke="url(#DecoLeftGradient)"
+        strokeWidth="4"
+      />
+      <path
+        d={`M0 18 H ${angleX} L ${width} 180`}
+        fill="transparent"
+        stroke="url(#DecoLeftGradient)"
+        strokeWidth="4"
+      />
+      <path
+        d={`M0 33 H ${angleX} L ${width} 195`}
+        fill="transparent"
+        stroke="url(#DecoLeftGradient)"
+        strokeWidth="4"
+      />
+    </svg>
+  );
+};
--- packages/kitae-designer/src/pages/Login.tsx
+++ packages/kitae-designer/src/pages/Login.tsx
@@ -1,22 +1,18 @@
 import { GitlabIcon } from "../assets/GitlabIcon";
 import { GitHubIcon } from "../assets/GithubIcon";
 import { LoginButton } from "../components/LoginButton";
-import { DecoLineBottomLeft } from "../assets/DecoLineBottomLeft";
-import { DecoLineMiddleLeft } from "../assets/DecoLineMiddleLeft";
-import { DecoLineTopLeft } from "../assets/DecoLineTopLeft";
 import { KitaeBox } from "../components/KitaeBox";
+import { DecoLeft } from "../assets/DecoLeft";
+import { DecoRight } from "../assets/DecoRight";
 
 export const Login = () => (
   <>
-    {/* <div className="absolute">
-      <DecoLineBottomLeft />
+    <div className="absolute top-[25%] left-[50%] translate-x-[-100%] -z-[0]">
+      <DecoLeft />
     </div>
-    <div className="absolute">
-      <DecoLineMiddleLeft />
+    <div className="absolute top-[42%] right-[50%] translate-x-[100%] -z-[0]">
+      <DecoRight />
     </div>
-    <div className="absolute">
-      <DecoLineTopLeft />
-    </div> */}
     <KitaeBox>
       <div className="flex flex-col items-center gap-2">
         <h1 className="text-4xl font-extralight">Log in to Kitae</h1>

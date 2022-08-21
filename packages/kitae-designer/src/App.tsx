d387e896de5f480ba050e3840b51c751c75542fc
lheido <leonard.treille@gmail.com>
2022-08-21 20:19:33 +0000

    feat(designer): WIP Gitlab log in feature

--- packages/kitae-designer/src/App.tsx
+++ packages/kitae-designer/src/App.tsx
@@ -1,6 +1,8 @@
+import { supabase } from "./features/supabase";
 import { Login } from "./pages/Login";
 
 function App() {
+  console.log(supabase.auth.user());
   return <Login />;
 }
 
--- packages/kitae-designer/src/pages/Login.tsx
+++ packages/kitae-designer/src/pages/Login.tsx
@@ -4,30 +4,45 @@ import { LoginButton } from "../components/LoginButton";
 import { KitaeBox } from "../components/KitaeBox";
 import { DecoLeft } from "../assets/DecoLeft";
 import { DecoRight } from "../assets/DecoRight";
+import { supabase } from "../features/supabase";
 
-console.log(import.meta.env.KITAE_SUPABASE_ANON_KEY);
-
-export const Login = () => (
-  <>
-    <div className="absolute top-[25%] left-[50%] translate-x-[-100%] -z-[0]">
-      <DecoLeft />
-    </div>
-    <div className="absolute top-[42%] right-[50%] translate-x-[100%] -z-[0]">
-      <DecoRight />
-    </div>
-    <KitaeBox>
-      <div className="flex flex-col items-center gap-2">
-        <h1 className="text-4xl font-extralight">Log in to Kitae</h1>
-        <p>いらっしゃいませ</p>
+export const Login = () => {
+  const gitlabLogin = async () => {
+    try {
+      const { user, session, error } = await supabase.auth.signIn({
+        provider: "gitlab",
+      });
+      if (error) throw error;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <div className="absolute top-[25%] left-[50%] translate-x-[-100%] -z-[0]">
+        <DecoLeft />
       </div>
-      <ul className="flex flex-col gap-3">
-        <li>
-          <LoginButton icon={<GitlabIcon />} label="Continue with Gitlab" />
-        </li>
-        <li>
-          <LoginButton icon={<GitHubIcon />} label="Continue with GitHub" />
-        </li>
-      </ul>
-    </KitaeBox>
-  </>
-);
+      <div className="absolute top-[42%] right-[50%] translate-x-[100%] -z-[0]">
+        <DecoRight />
+      </div>
+      <KitaeBox>
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-4xl font-extralight">Log in to Kitae</h1>
+          <p>いらっしゃいませ</p>
+        </div>
+        <ul className="flex flex-col gap-3">
+          <li>
+            <LoginButton
+              onClick={() => gitlabLogin()}
+              icon={<GitlabIcon />}
+              label="Continue with Gitlab"
+            />
+          </li>
+          <li>
+            <LoginButton icon={<GitHubIcon />} label="Continue with GitHub" />
+          </li>
+        </ul>
+      </KitaeBox>
+    </>
+  );
+};

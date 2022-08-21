49fd7fb5cb54c487060240abbae8dd71fdfdedc2
lheido <leonard.treille@gmail.com>
2022-08-21 14:23:43 +0000

    feat(compiler): WIP react component templates

--- packages/kitae-designer/src/components/LoginButton.tsx
+++ packages/kitae-designer/src/components/LoginButton.tsx
@@ -1,5 +1,3 @@
-import { ReactComponentElement } from "react";
-
 export const LoginButton = (props: { label: string; icon: JSX.Element }) => (
   <button className="relative flex flex-start gap-2 p-2 w-full rounded bg-neutral-700 kitae-gradient">
     <span className="relative">{props.icon}</span>

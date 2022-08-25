9c43a0b75db898716b6541e9133a17fc22715db9
lheido <leonard.treille@gmail.com>
2022-08-25 12:11:28 +0000

    chore: Reorganize workspaces

---
+++ packages/designer/server/jest.config.ts
@@ -0,0 +1,8 @@
+import { Config } from "@jest/types";
+import projectConfig from "../../../jest.config";
+
+const config: Config.InitialOptions = {
+  ...projectConfig,
+};
+
+export default config;

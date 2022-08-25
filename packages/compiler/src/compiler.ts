7835f20cc1c4805c6204adb573e3609a92176995
lheido <leonard.treille@gmail.com>
2022-08-25 20:38:15 +0000

    feat(compiler): WIP start react compiler

--- packages/compiler/src/compiler.ts
+++ packages/compiler/src/compiler.ts
@@ -1,16 +1,22 @@
-import { KitaeAST } from "./model";
+// import { KitaeAST } from "./model";
 
-export function transform(ast: KitaeAST): string {
-  return "";
+// export function transform(ast: KitaeAST): string {
+//   return "";
+// }
+
+// const ASTExample = {
+//   type: "react:component",
+//   inputs: {},
+//   styles: {
+//     padding: "1rem",
+//     className: "p-1 bg-teal-500 rounded", //tailwind support ?
+//     backgroundColor: "black",
+//   },
+//   children: [],
+// };
+
+export function compile(ast: any): any {
+  // TODO: detect the driver,
+  // react driver use jsx instead of template property if possible.
+  return null;
 }
-
-const ASTExample = {
-  type: "react:component",
-  inputs: {},
-  styles: {
-    padding: "1rem",
-    className: "p-1 bg-teal-500 rounded", //tailwind support ?
-    backgroundColor: "black",
-  },
-  children: [],
-};

1e0810b864343bf11fdd0647dcc39e4c5015082a
lheido <leonard.treille@gmail.com>
2022-08-26 08:05:01 +0000

    Update packages/compiler/src/compiler.test.ts

--- packages/compiler/src/compiler.test.ts
+++ packages/compiler/src/compiler.test.ts
@@ -1,44 +1,56 @@
 import { compile } from "./compiler";
 
 const ast = {
-  pages: [{ label: "Page number 1", id: "Page_1" }],
-  Page_1: {
-    type: "component",
-    driver: "react",
-    template: [
-      {
-        type: "container",
-        className: "flex gap-1",
-        children: [
-          {
-            type: "paragraph",
-            semantic: "p",
-            children: [
-              {
-                type: "text",
-                content: "Lorem ipsum",
-              },
-              {
-                type: "container",
-                semantic: "span",
-                children: [
-                  {
-                    type: "text",
-                    content: "Lorem ipsum",
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
+  pages: [{ label: "Page number 1", id: "page_1" }],
+  refs: {
+    page_1: {
+      type: "component",
+      driver: "react",
+      template: [
+        {
+          type: "container",
+          className: "flex gap-1",
+          children: [
+            {
+              type: "paragraph",
+              semantic: "p",
+              children: [
+                {
+                  type: "text",
+                  content: "Lorem ipsum",
+                },
+                {
+                  type: "button",
+                  className: 'flex p-2 bg-teal-500',
+                  children: [
+                    {
+                      type: "text",
+                      content: "Button amet",
+                    },
+                  ]
+                },
+                {
+                  type: "container",
+                  semantic: "span",
+                  children: [
+                    {
+                      type: "text",
+                      content: "Lorem ipsum",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }
+  }
 };
 
 describe("compiler", () => {
   it("should compile the ast to a set of files", async () => {
-    const result = compile(ast.Page_1);
+    const result = compile(ast.refs.page_1);
     expect(result.files.length).toEqual(1);
   });
 });

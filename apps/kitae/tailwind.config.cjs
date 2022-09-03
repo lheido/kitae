791e3fb455ecd1e7576928a69eda641f083f88ee
lheido <leonard.treille@gmail.com>
2022-09-03 12:34:12 +0000

    chore(kitae): WIP reorganize repo - Move designer front to the kitae app

---
+++ apps/kitae/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "kitae-start": "#1D8660",
+        "kitae-end": "#1D6686",
+      },
+    },
+  },
+  plugins: [],
+};

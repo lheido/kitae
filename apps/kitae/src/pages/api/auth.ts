fb46cd57d6f7269ebe141635fb1ccbfc1a7577b2
lheido <leonard.treille@gmail.com>
2022-09-07 11:57:33 +0000

    chore(kitae): Another try...

--- apps/kitae/src/pages/api/auth.ts
+++ apps/kitae/src/pages/api/auth.ts
@@ -1,11 +1,12 @@
-import { serialize } from "cookie";
+import { parse, serialize } from "cookie";
 import { supabase } from "../../features/supabase";
 
 export class ResponseProxy {
   constructor(public res: Response) {}
 
-  setHeader(name: string, value: string) {
-    this.res.headers.set(name, value);
+  setHeader(name: string, value: string | string[]) {
+    const _value = Array.isArray(value) ? value : [value];
+    _value.forEach((val) => this.res.headers.append(name, val));
     return this;
   }
 
@@ -42,15 +43,38 @@ export async function post({ request }: { request: Request }) {
   });
   const body = await request.json();
   const req = { ...request, body, headers };
-  supabase.auth.api.setAuthCookie(req, proxy);
-  // proxy.res.headers.append(
-  //   "Set-Cookie",
-  //   serialize("kitae-refresh-token", body.session.refresh_token, {
-  //     domain: ".gitpod.io",
-  //     httpOnly: true,
-  //     sameSite: "lax",
-  //     maxAge: 360000,
-  //   })
-  // );
+  // supabase.auth.api.setAuthCookie(req, proxy);
+  console.log("------START------");
+  console.log("------------");
+  proxy.setHeader(
+    "Set-Cookie",
+    [
+      {
+        name: "kitae-access-token",
+        value: body.session.access_token,
+        options: {
+          domain: ".gitpod.io",
+          path: "/",
+          httpOnly: true,
+          sameSite: "lax",
+          maxAge: 300,
+        },
+      },
+      {
+        name: "kitae-refresh-token",
+        value: body.session.refresh_token,
+        options: {
+          domain: ".gitpod.io",
+          path: "/",
+          httpOnly: true,
+          sameSite: "lax",
+          maxAge: 360000,
+        },
+      },
+    ].map((c) => serialize(c.name, c.value, c.options as any))
+  );
+  console.log("------------");
+  console.log(proxy.res.headers.get("Set-Cookie"));
+  console.log("-----END-------");
   return proxy.res;
 }

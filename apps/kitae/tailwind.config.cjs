71747980f980d8196ffeedd7302469a2d7cbcda2
lheido <leonard.treille@gmail.com>
2022-09-10 17:27:07 +0000

    style(kitae): Update dashboard style

--- apps/kitae/tailwind.config.cjs
+++ apps/kitae/tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
       colors: {
         "kitae-start": "#1D8660",
         "kitae-end": "#1D6686",
+        "kitae-container": "#131313",
+        "kitae-separator": "#454545",
+        "kitae-text": "#E6E6E6",
+        "kitae-text-inverse": "#2A2A2A",
+        "kitae-panel-bg": "#2B2B2B",
+        "kitae-panel": "#292929",
+        "kitae-icon": "#787878",
       },
     },
   },

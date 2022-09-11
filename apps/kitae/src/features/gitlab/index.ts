e88fa6667346b4ac7c445748458b14e1aeee1bad
lheido <leonard.treille@gmail.com>
2022-09-11 11:03:18 +0000

    feat(kitae): Refacor getUserProjects

--- apps/kitae/src/features/gitlab/index.ts
+++ apps/kitae/src/features/gitlab/index.ts
@@ -1,5 +1,16 @@
-import { Users } from "@gitbeaker/node";
+import { Projects, Users } from "@gitbeaker/node";
+import type { Types } from "@gitbeaker/core";
+import type { Session } from "@supabase/supabase-js";
 
-export function createAPi(oauthToken: string) {
-  return new Users({ oauthToken });
+export function createApi(oauthToken: string) {
+  return new Projects({ oauthToken });
+}
+
+export async function getUserProjects(
+  session: Session,
+  options: Types.PaginatedRequestOptions = {}
+) {
+  if (!session.provider_token) throw new Error("Provider token not found.");
+  const api = createApi(session.provider_token);
+  return api.all({ membership: true, ...options });
 }

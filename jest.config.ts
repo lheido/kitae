b24cd761f6e38c02410f04d0cd4a846d16fce551
lheido <leonard.treille@gmail.com>
2023-01-19 16:30:04 +0100

    Remove all

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from "@jest/types";
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  passWithNoTests: true,
-};
-
-export default config;
--- packages/designer/front/src/components/KitaeBox.tsx
+++
@@ -1,7 +0,0 @@
-export const KitaeBox = (props: { children: JSX.Element[] | JSX.Element }) => (
-  <div className="relative p-1 bg-gradient-to-r from-kitae-start to-kitae-end rounded-2xl w-[339px] h-[399px] flex">
-    <div className="flex flex-col justify-center items-center gap-11 bg-neutral-900 rounded-2xl flex-1">
-      {props.children}
-    </div>
-  </div>
-);
